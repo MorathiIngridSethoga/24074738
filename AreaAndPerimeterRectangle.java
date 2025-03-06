cf6b82a51beadd7ceac1f25e493a17c9c6d8ec15
MorathiIngridSethoga <24074738@mvula.univen.ac.za>
2025-03-06 19:04:54 +0200

    Add files via upload

---
+++ AreaAndPerimeterRectangle.java
@@ -0,0 +1,16 @@
+
+public class AreaAndPerimeterRectangle {
+    
+    public static void main(String[] args) {
+        
+        double Area , Perimeter;
+        double length = 4.9;
+            double breath =8.20;
+                
+     Area = length*breath;
+Perimeter = 2*(length)+2*(breath);     
+        System.out.println("Area is" + Area);
+        System.out.println("Perimeter is" + Perimeter);
+    }
+    
+}
